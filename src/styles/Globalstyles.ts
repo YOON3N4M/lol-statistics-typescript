fe6d9ac820887b60c6add51ef6068a15823abfc2
윤세남 <lgmysn2@gmail.com>
2023-10-24 10:18:00 +0900

    styels: 글로벌 스타일 분리

---
+++ src/styles/Globalstyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  body{
+    padding:0;
+    margin:0;
+    background-color: #ebeef1;
+  }
+  ul{
+    padding: 0 0;
+    margin: 0px
+  }
+  li{
+    list-style: none;
+  }
+
+`
